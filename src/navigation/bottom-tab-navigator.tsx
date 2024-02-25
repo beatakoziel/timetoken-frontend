bd2bd347eef8f56daa7cf3be8bcc4434d1dceefc
beatakoziel <beata.koziel@symmetrical.ai>
2024-02-25 16:37:57 +0100

    Changed definitions icon

--- src/navigation/bottom-tab-navigator.tsx
+++ src/navigation/bottom-tab-navigator.tsx
@@ -1,26 +1,30 @@
 import Icons from "@/components/shared/icons"
 import CompletedScreen from "@/screens/completed-screen"
 import TodayScreen from "@/screens/today-screen"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import { useTheme } from "@shopify/restyle"
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import {useTheme} from "@shopify/restyle"
 import CategoriesStackNavigator from "./categories-stack-navigator"
 import HomeStackNavigator from "./home-stack-navigator"
-import { RootBottomTabParamList } from "./types"
+import {RootBottomTabParamList} from "./types"
 import DefinitionsScreen from "@/screens/definitions-screen"
+import IconButton from "@/components/shared/icon-button";
+import {Ionicons} from "@expo/vector-icons";
+import React from "react";
+import {palette} from "@/utils/theme/colors";
 
 const Tab = createBottomTabNavigator<RootBottomTabParamList>()
 
 const BottomTabNavigator = () => {
-  const theme = useTheme()
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: "black",
-        tabBarInactiveTintColor: theme.colors.gray550,
-        tabBarHideOnKeyboard: true,
-      }}
-    >
-      {/* <Tab.Screen
+    const theme = useTheme()
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarActiveTintColor: "black",
+                tabBarInactiveTintColor: theme.colors.gray550,
+                tabBarHideOnKeyboard: true,
+            }}
+        >
+            {/* <Tab.Screen
         name="HomeStack"
         component={HomeStackNavigator}
         options={() => ({
@@ -29,7 +33,7 @@ const BottomTabNavigator = () => {
           headerShown: false,
         })}
       /> */}
-      {/* <Tab.Screen
+            {/* <Tab.Screen
         name="Completed"
         component={CompletedScreen}
         options={() => ({
@@ -38,16 +42,16 @@ const BottomTabNavigator = () => {
           headerShown: false,
         })}
       /> */}
-      <Tab.Screen
-        name="Definitions"
-        component={DefinitionsScreen}
-        options={() => ({
-          title: "Definitions",
-          tabBarIcon: ({ color }) => <Icons name="completed" color={color} />,
-          headerShown: false,
-        })}
-      />
-      {/* <Tab.Screen
+            <Tab.Screen
+                name="Definitions"
+                component={DefinitionsScreen}
+                options={() => ({
+                    title: "Definitions",
+                    tabBarIcon: ({color}) => <Ionicons name="library-outline" size={30} color={palette.gray600}/>,
+                    headerShown: false,
+                })}
+            />
+            {/* <Tab.Screen
         name="Today"
         component={TodayScreen}
         options={() => ({
@@ -66,8 +70,8 @@ const BottomTabNavigator = () => {
           headerShown: false,
         })}
       /> */}
-    </Tab.Navigator>
-  )
+        </Tab.Navigator>
+    )
 }
 
 export default BottomTabNavigator
